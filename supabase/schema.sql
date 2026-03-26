19c55a5088ca488a61de1d9bc131f1f34b82d7e8
Aqua Hoshino <kronacollective+aqua@gmail.com>
2026-03-26 15:29:28 +0100

    Update database schema

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -9,8 +9,16 @@ CREATE TABLE public.accounts (
   display_name text,
   description text,
   color text,
+  sp_key text,
   CONSTRAINT accounts_pkey PRIMARY KEY (id),
   CONSTRAINT accounts_user_fkey FOREIGN KEY (user) REFERENCES auth.users(id)
+);
+CREATE TABLE public.bucket_fields (
+  bucket uuid NOT NULL,
+  field uuid NOT NULL,
+  CONSTRAINT bucket_fields_pkey PRIMARY KEY (bucket, field),
+  CONSTRAINT bucket_fields_bucket_fkey FOREIGN KEY (bucket) REFERENCES public.buckets(id),
+  CONSTRAINT bucket_fields_field_fkey FOREIGN KEY (field) REFERENCES public.fields(id)
 );
 CREATE TABLE public.bucket_friends (
   bucket uuid NOT NULL,
@@ -34,6 +42,40 @@ CREATE TABLE public.buckets (
   color text DEFAULT '255, 255, 255'::text,
   CONSTRAINT buckets_pkey PRIMARY KEY (id),
   CONSTRAINT buckets_account_fkey FOREIGN KEY (account) REFERENCES public.accounts(id)
+);
+CREATE TABLE public.field_values (
+  field uuid NOT NULL DEFAULT gen_random_uuid(),
+  member text NOT NULL,
+  value json,
+  CONSTRAINT field_values_pkey PRIMARY KEY (field, member),
+  CONSTRAINT field_values_field_fkey FOREIGN KEY (field) REFERENCES public.fields(id),
+  CONSTRAINT field_values_member_fkey FOREIGN KEY (member) REFERENCES public.members(id)
+);
+CREATE TABLE public.fields (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  account uuid NOT NULL,
+  name text NOT NULL,
+  type text NOT NULL DEFAULT 'text'::text,
+  CONSTRAINT fields_pkey PRIMARY KEY (id),
+  CONSTRAINT fields_account_fkey FOREIGN KEY (account) REFERENCES public.accounts(id)
+);
+CREATE TABLE public.folder_members (
+  folder uuid NOT NULL,
+  member text NOT NULL DEFAULT ''::text,
+  CONSTRAINT folder_members_pkey PRIMARY KEY (folder, member),
+  CONSTRAINT folder_members_folder_fkey FOREIGN KEY (folder) REFERENCES public.folders(id),
+  CONSTRAINT folder_members_member_fkey FOREIGN KEY (member) REFERENCES public.members(id)
+);
+CREATE TABLE public.folders (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  name text,
+  description text,
+  color text,
+  subfolder_of uuid,
+  account uuid NOT NULL,
+  CONSTRAINT folders_pkey PRIMARY KEY (id),
+  CONSTRAINT folders_subfolder_of_fkey FOREIGN KEY (subfolder_of) REFERENCES public.folders(id),
+  CONSTRAINT folders_account_fkey FOREIGN KEY (account) REFERENCES public.accounts(id)
 );
 CREATE TABLE public.friends (
   relating uuid NOT NULL,
@@ -64,6 +106,13 @@ CREATE TABLE public.members (
   color text NOT NULL DEFAULT '0, 0, 0'::text,
   account uuid,
   is_status boolean NOT NULL DEFAULT false,
+  archived boolean NOT NULL DEFAULT false,
   CONSTRAINT members_pkey PRIMARY KEY (id),
   CONSTRAINT members_account_fkey FOREIGN KEY (account) REFERENCES public.accounts(id)
+);
+CREATE TABLE public.subscriptions (
+  account uuid NOT NULL,
+  subscription jsonb NOT NULL,
+  CONSTRAINT subscriptions_pkey PRIMARY KEY (account),
+  CONSTRAINT subscriptions_account_fkey FOREIGN KEY (account) REFERENCES public.accounts(id)
 );
